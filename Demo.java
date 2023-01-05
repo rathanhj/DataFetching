e193dc8b659f0193a60d8917caefd33956095680
Rathan H J <121946937+rathanhj@users.noreply.github.com>
2023-01-05 16:22:29 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,28 @@
+package hybernate;
+
+import java.sql.*;
+
+public class Demo {
+
+	public static void main(String[] args) throws SQLException
+	{
+		// TODO Auto-generated method stub
+		
+		String url = "jdbc:mysql://localhost:3306/rathandb";
+		String uname = "root";
+		String pass = "Rathan7*";
+		String query = "select FirstName from employee where EmpID=2";
+
+		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());;
+		//Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, uname, pass);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		rs.next();
+		String name = rs.getString("FirstName");
+		System.out.println(name);
+		st.close();
+		con.close();
+		}
+	//jdbc:mysql://localhost:3306/?user=root
+}
